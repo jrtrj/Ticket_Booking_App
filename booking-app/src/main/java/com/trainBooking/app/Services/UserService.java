b83ecfa3e791038767a780613e71c759c22da00b
jrtrj <jerit.reji.dev@gmail.com>
2025-07-09 17:27:52 +0530

    user login

--- booking-app/src/main/java/com/trainBooking/app/Services/UserService.java
+++ booking-app/src/main/java/com/trainBooking/app/Services/UserService.java
@@ -4,27 +4,37 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.File;
 import java.util.List;
+import java.util.Optional;
+
+import com.trainBooking.app.Util.AuthUtil;
 import com.trainBooking.app.entities.User;
 
 public class UserService {
     private static final String userDBPath = "src/main/java/com/trainBooking/app/LocalDB/User.json";
-    private User user;
+    private User loggedInUser;
     private JsonDBService jsonDBService;
     private List<User> userList;
     public UserService() {}
-    public UserService(User user, JsonDBService jsonDBService) {
-        this.user = user;
+    public UserService(JsonDBService jsonDBService) {
         this.jsonDBService = jsonDBService;
         readUserFromDB(userDBPath);
     }
     public void readUserFromDB(String userDBPath) {
         File path = new File(userDBPath);
-        userList = jsonDBService.readAll(path,new TypeReference<<List User>(){});
+        userList = jsonDBService.readAll(path,new TypeReference<List<User>>(){});
     }
-    public Boolean loginUser() {
-        Boolean isPresent = userList.stream()
-                                    .map(u -> u.getUserId())
-                                    .filter(id -> user.getUserId().equalsIgnoreCase(id)
-                                            && AuthenticationService.checkPassword())
+    public Boolean loginUser(String email, String password) {
+        Optional<User> foundUser = userList
+                                    .stream()
+                                    .filter(u -> u.getEmail().equals(email))
+                                    .findFirst();
+        if(foundUser.isPresent()) {
+            User user = foundUser.get();
+            if (AuthUtil.verifyPasswd(password,foundUser.get().getHashedPassword())) {
+                loggedInUser = user;
+                return true;
+            }
+        }
+        return false;
     }
 }
