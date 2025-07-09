739aef97df43bedf569b756a11ebe0d455c90ce0
jrtrj <jerit.reji.dev@gmail.com>
2025-07-09 22:34:39 +0530

    user signup

--- booking-app/src/main/java/com/trainBooking/app/Services/UserService.java
+++ booking-app/src/main/java/com/trainBooking/app/Services/UserService.java
@@ -3,6 +3,7 @@ package com.trainBooking.app.Services;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,19 +11,24 @@ import com.trainBooking.app.Util.AuthUtil;
 import com.trainBooking.app.entities.User;
 
 public class UserService {
-    private static final String userDBPath = "src/main/java/com/trainBooking/app/LocalDB/User.json";
+    private static final File userDBPath = new File("src/main/java/com/trainBooking/app/LocalDB/User.json");
     private User loggedInUser;
     private JsonDBService jsonDBService;
     private List<User> userList;
     public UserService() {}
     public UserService(JsonDBService jsonDBService) {
         this.jsonDBService = jsonDBService;
-        readUserFromDB(userDBPath);
+        try {
+            readUserFromDB();
+        } catch (IOException e) {
+            System.err.println("Could not load users during service init" + e.getMessage());
+        }
     }
-    public void readUserFromDB(String userDBPath) {
-        File path = new File(userDBPath);
-        userList = jsonDBService.readAll(path,new TypeReference<List<User>>(){});
+
+    public void readUserFromDB() throws IOException {
+        userList = jsonDBService.readAll(userDBPath,new TypeReference<List<User>>(){});
     }
+
     public Boolean loginUser(String email, String password) {
         Optional<User> foundUser = userList
                                     .stream()
@@ -37,4 +43,26 @@ public class UserService {
         }
         return false;
     }
+    public Boolean signUp(User newUser) {
+        try {
+            Optional<User> existingUser = userList
+                    .stream()
+                    .filter(user -> user.getEmail().equalsIgnoreCase(newUser.getEmail()))
+                    .findFirst();
+            if(existingUser.isPresent())
+                return false;
+            userList.add(newUser);
+            jsonDBService.writeAll(userDBPath,userList);
+            return true;
+        }catch (IOException e) {
+            System.err.println("Could not write into db during signUp "+e.getMessage());
+        }
+    }
 }
+//                userList.add(loggedInUser);
+//                try {
+//                        jsonDBService.writeAll(userDBPath, userList);
+//                }catch(IOException e) {
+//        System.err.println("loginUser failed at writing to DB" + e.getMessage());
+//        }
+//
